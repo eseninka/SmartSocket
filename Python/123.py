082d01fbeccccd3ca118013e36756e0d30408cb8
eseninka <eseninka2706@gmail.com>
2026-02-06 23:30:01 +0300

    start work with postrgesql

--- Python/123.py
+++ Python/123.py
@@ -1,25 +1,13 @@
 import paho.mqtt.client as mqtt
 import json
 from class_roz import obrabotka
+import psycopg2
 
-# def parse_turple(st):
-#     a, b = st[1:-1].split(',')
-#     return (a,b)
-
-# topic_lists = ['kvant/R22/BV/amper', 'kvant/R22/BV/voltage']
-# amper_list = []
-# voltage_list = []
-
-
-# def device_classifare(amp, volt):
-#     amp = np.array(amp)
-#     volt = np.array(volt)
-#     I_rms = np.sqrt(np.mean(amp**2))
-#     U_rms = np.sqrt(np.mean(volt**2))
-#     S = I_rms * U_rms
-#     P = np.mean(amp * volt)
-#     cosfi = P / S
-#     print(f'''I_rms:{I_rms}\nU_rms:{U_rms}\nS:{S}\nP:{P}\ncosfi:{cosfi}''')
+host = 'localhost'
+name_user = 'postgres'
+password = '123'
+database = 'SmartSocket'
+connect = '5432'
 
 obr = obrabotka()
 
@@ -37,10 +25,26 @@ def on_message(client, userdata, msg):
     print(obr.Q)
     print(obr.S)
     print(obr.P)
+    print(obr.I_rms)
+    print(obr.U_rms)
+    print(obr.P_rms)
+    #client.publish("kvant/R22/BV/test", str(data['rms_A']))
+    try:
+        connection = psycopg2.connect(host=host, user=name_user, password=password, database=database)
+        connection.autocommit = True
+        with connection.cursor() as cursor:
+            cursor.execute('insert into data_current(uuid, flash_current, rms_current) values (%s, %s, %s)', (data['uuid'], str(data['amper']['data']), str(data['rms_A'])))
+    except Exception as e:
+        print(f'[info]: Ошибка {e}')
+    finally:
+        if connection:
+            connection.close()
+            print('[info]: коннект закрыт')
 
 client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
 client.on_message = on_message  # Обработчик сообщений
 client.username_pw_set("Rasbery", "154321")  # Логин и пароль
 client.connect("m5.wqtt.ru", 14182)  # Подключение
-client.subscribe('kvant/R22/BV/update')  # Подписка на ваш топик
+client.subscribe('kvant/R22/BV/update')  # Подписка на топик
+
 client.loop_forever()  # Запуск цикла
