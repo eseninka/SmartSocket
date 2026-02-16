3162f972d34a970a3655b3ff6950c9969626fd7b
eseninka <eseninka2706@gmail.com>
2026-02-16 23:56:23 +0300

    edit directory with SQL

---
+++ SQL/main_tables.sql
@@ -0,0 +1,28 @@
+create table calculation
+(
+num_measurements serial not null,
+uuid text               not null,
+time                    timestamp default current_timestamp,
+P                       real not null,
+I                       real not null,
+V                       real not null,
+cosfi                   real not null
+)
+
+create table data_current
+(
+num_measurements serial not null,
+uuid text               not null,
+time                    timestamp default current_timestamp,
+flash_current           text not null,
+rms_current             real not null
+)
+
+create table data_voltage
+(
+num_measurements serial not null,
+uuid text               not null,
+time                    timestamp default current_timestamp,
+flash_voltage           text not null,
+rms_voltage             real not null
+)
